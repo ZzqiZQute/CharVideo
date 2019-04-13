300cdc917e972d4d65df39abf35f1151f44f9ea7
ZzqiZQute <zhaozq11th@ustbsmartcar.org>
2019-04-13 18:34:51 +0800

    fix:play no sound video without mpv

--- play.py
+++ play.py
@@ -7,10 +7,10 @@ import time
 mpvpath1='/usr/bin/mpv'
 mpvpath2='/usr/local/bin/mpv'
 mpvpath3='./mpv'
-
+hassound=True
 if not os.path.exists(mpvpath1) and not os.path.exists(mpvpath2) and not os.path.exists(mpvpath3):
-    print("脚本依赖应用程序mpv，请安装mpv后再运行该脚本")
-    exit(1)
+    print("脚本依赖应用程序mpv，如果需要播放声音，请安装mpv后再运行该脚本")
+    hassound=False
 null = open('/dev/null', 'w')
 parser = configparser.ConfigParser()
 parser.read('./settings.conf')
@@ -22,7 +22,8 @@ height = parser['General']['height']
 print("请将终端大小调至%sx%s,并按回车键继续" % (width, int(height)+1))
 input()
 os.system('clear')
-sound = subprocess.Popen(['mpv', './sound.aac'], stdout=null, stderr=null)
+if hassound:
+    sound = subprocess.Popen(['mpv', './sound.aac'], stdout=null, stderr=null)
 fileidx = 0
 while True:
     currenttime = time.time()
